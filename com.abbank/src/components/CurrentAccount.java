a50f88a67893b0a95f8956a6151f34a31a39d13b
André Brito <awbrito@gmail.com>
2023-11-23 12:52:04 +0000

    // 1.2.2 Creation of the CurrentAccount and SavingsAccount

---
+++ com.abbank/src/components/CurrentAccount.java
@@ -0,0 +1,16 @@
+package components; // 1.2.2 Creation of the CurrentAccount and SavingsAccount
+
+//Inherits from Account class
+public class CurrentAccount extends Account {
+	//Constructor
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
+	//only attributes from Account class
+    
+	// Implementation of the abstract method from the Account class
+    @Override
+    public void setBalance(double amount) {
+    	this.balance = amount;
+    }
+}
